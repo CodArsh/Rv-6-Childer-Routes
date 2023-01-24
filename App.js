3d956825d51b80d812a9a10df686cc179af65a24
Mohammad Arsil <95223801+CodArsh@users.noreply.github.com>
2023-01-24 09:06:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+// import Navbar from "./Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./CMP/Signup/Signup";
+import Admin from "./CMP/Admin/Admin";
+import Login from "./CMP/Login/Login";
+import NotFound from "./CMP/NotFound/NotFound";
+import Dashboard from "./CMP/Admin/Dashboard/Dashboard";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Homepage</h1>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/admin-panel" element={<Admin />}>
+            <Route path="dashboard" element={<Dashboard />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
+# command
+  npm i react-router-dom
