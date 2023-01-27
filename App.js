3e1d4cc9cdcc7b7c5af9c53a3a1eead17a1ce0dc
Mohammad Arsil <95223801+CodArsh@users.noreply.github.com>
2023-01-27 16:56:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,26 +7,52 @@ import Admin from "./CMP/Admin/Admin";
 import Login from "./CMP/Login/Login";
 import NotFound from "./CMP/NotFound/NotFound";
 import Dashboard from "./CMP/Admin/Dashboard/Dashboard";
+import { ThemeProvider, createTheme } from "@mui/material";
+import {
+  deepPurple,
+  deepOrange,
+  cyan,
+  teal,
+  pink,
+  lightBlue,
+} from "@mui/material/colors";
+import "@fontsource/source-sans-pro/400.css";
 function App() {
+  const Theme = createTheme({
+    palette: {
+      primary: deepPurple,
+      secondary: teal,
+      error: pink,
+      warning: deepOrange,
+      success: cyan,
+      info: lightBlue,
+    },
+    typography: {
+      fontFamily: "Source Sans Pro",
+    },
+  });
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<h1>Homepage</h1>} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin-panel" element={<Admin />}>
-            <Route path="dashboard" element={<Dashboard />} />
+      <ThemeProvider theme={Theme}>
+        <Router>
+          <Routes>
+            {/* <Route path="/" element={<h1>Homepage</h1>} /> */}
+            <Route path="/" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin-panel" element={<Admin />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
 
 export default App;
+
 
 
 # command
